68b25a89795e178dfed5091eb0c2f5c931d43807
Igor Mineev <igron99@mail.ru>
2024-05-05 12:36:18 +0300

    Base

--- xml2md.py
+++ xml2md.py
@@ -1,16 +1,24 @@
 import xml.etree.ElementTree as ET
 
 
-def xml2md(elem: ET.Element):
+def xml2md(elem: ET.Element, idx=0):
     if elem.tag == 'p':
         return ((elem.text if elem.text else '') +
                 ''.join(xml2md(inner) for inner in elem) +
+                (elem.tail if elem.tail else '') + '\n')
+    if elem.tag == 'ul':
+        return ((elem.text if elem.text else '') + '\n' +
+                ''.join(xml2md(inner, i) for i, inner in enumerate(elem)) +
+                (elem.tail if elem.tail else '') + '\n')
+    if elem.tag == 'li':
+        return (f'{idx + 1}) ' + (elem.text if elem.text else '') +
+                ''.join(xml2md(inner, i) for i, inner in enumerate(elem)) +
                 (elem.tail if elem.tail else '') + '\n')
     if elem.tag == 'pre':
         return '```\n' + ''.join(elem.itertext()) + '\n```' + (elem.tail if elem.tail else '')
     if elem.tag == 'b':
         return '**' + ''.join(elem.itertext()) + '**' + (elem.tail if elem.tail else '')
-    if elem.tag == 'tt':
+    if elem.tag == 'tt' or elem.tag == 'code':
         return '`' + ''.join(elem.itertext()) + '`' + (elem.tail if elem.tail else '')
     raise RuntimeError(f"Unsupported {elem}")
 
